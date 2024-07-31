7c5fe9d4feef0d75aad76827807b731b10e5481d
Sergio Aquino <serg.garcia.2209@gmail.com>
2024-07-31 00:20:03 -0600

    Se inicia proyecto

---
+++ CODIGO_FUENTE/jakarta-rest-demo/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.serchcodev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
